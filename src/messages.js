31b931bfa14492d8e79721ebf9ae8555bda54b78
leandrokanis <leandro.alves@g44.com.br>
2019-04-12 12:40:37 -0300

    getting hasOwnProperty back

--- src/messages.js
+++ src/messages.js
@@ -49,7 +49,7 @@ Messages.prototype = {
     var name = attribute;
     if (this.attributeNames.hasOwnProperty(attribute)) {
       return this.attributeNames[attribute];
-    } else {
+    } else if (this.messages.attributes.hasOwnProperty(attribute)) {
       name = this.messages.attributes[attribute];
     }
 
